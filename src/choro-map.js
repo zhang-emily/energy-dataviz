d9f23bcb7e22efe8a6bcb5a6867f16c357d05896
“zhang-emily” <ezhang93@gmail.com>
2021-03-17 17:54:59 -0500

    stuck, going to try a different approach

--- src/choro-map.js
+++ src/choro-map.js
@@ -1,39 +1,75 @@
 import * as d3 from "d3";
 import * as topojson from "topojson-client";
+import { select } from "d3-selection";
+// import { legend } from "@d3/color-legend";
+
 // import { states } from "https://unpkg.com/us-atlas@1/us/10m.json";
 
 // -------- Choro map ---------- //
+
+// topojson = require("topojson-client@3");
+// d3 = require("d3@6");
 
 Promise.all([
   // d3.json("https://unpkg.com/us-atlas@1/us/10m.json"),
   d3.json("data/states-10m.json"),
   d3.csv("data/choro_2019.csv"),
 ]).then((results) => {
-  const [states, data] = results;
-  choroMap(states, data);
+  const [us, data] = results;
+  choroMap(us, data);
 });
 
 // set the dimensions and margins of the graph
-// var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-//   width = 600 - margin.left - margin.right,
-//   height = 400 - margin.top - margin.bottom;
+var margin = { top: 10, right: 30, bottom: 30, left: 60 };
+var width = 600 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
 
 var projection = d3.geoAlbersUsa();
 
-function choroMap(states, data) {
-  console.log(states);
-  const svg = d3
-    .create("svg")
+var sources = [
+  "Coal",
+  "Oil",
+  "Natural Gas",
+  "Solar",
+  "Wind",
+  "Hydro",
+  "Nuclear",
+];
+
+var color = d3
+  .scaleOrdinal()
+  .domain(sources)
+  .range([
+    "#323232",
+    "#634a4a",
+    "#eb9534",
+    "#f7eb45",
+    "cornflowerblue",
+    "#ab91ff",
+    "#e34646",
+  ]);
+
+function choroMap(us, data) {
+  // console.log(topojson.feature(us, us.objects.states).features);
+  console.log(data);
+
+  // var states = new Map(
+  //   us.objects.states.geometries.map((d) => [d.id, d.properties])
+  // );
+
+  var svg = d3
     .select("#choro-map")
-    .attr("viewBox", [0, 0, 975, 610]);
-
-  svg
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .selectAll("path")
-    .datum(topojson.feature(states, data).features)
-    .join("path")
-    .attr("fill", (d) => color(data.get(d.id)))
-    .attr("d", d3.geoPath().projection(projection));
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  return svg.node();
+  // svg
+  //   .append("g")
+  //   .selectAll("path")
+  //   .datum(topojson.feature(us, us.objects.states).features);
+  //   .join("path")
+  //   .attr("fill", (d) => color(data.get(d.Top_Source)))
+  //   .attr("d", d3.geoPath().projection(projection));
 }
